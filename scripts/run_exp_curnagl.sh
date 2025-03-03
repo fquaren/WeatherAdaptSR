758d29fb83b7ce9c1ecef1d0983e00e4d9464c60
Filippo Quarenghi <filippo.quarenghi@unil.ch>
2025-03-03 11:17:14 +0100

    testing

--- scripts/run_exp_curnagl.sh
+++ scripts/run_exp_curnagl.sh
@@ -4,9 +4,9 @@
 #SBATCH --mail-user filippo.quarenghi@unil.ch
 
 #SBATCH --chdir /scratch/fquareng/
-#SBATCH --job-name exp_c7
-#SBATCH --output outputs/UNet-Baseline/%j
-#SBATCH --error job_errors/UNet-Baseline/%j
+#SBATCH --job-name test
+#SBATCH --output outputs/%j
+#SBATCH --error job_errors/%j
 
 #SBATCH --partition gpu
 #SBATCH --gres gpu:1
@@ -15,11 +15,11 @@
 #SBATCH --ntasks 1
 #SBATCH --cpus-per-task 1
 #SBATCH --mem 10G
-#SBATCH --time 10:00:00
+#SBATCH --time 00:01:00
 
 module load singularityce/4.1.0
 export SINGULARITY_BINDPATH="/scratch,/dcsrsoft,/users,/work,/reference"
 singularity run --nv /dcsrsoft/singularity/containers/pytorch/pytorch-ngc-24.05-2.4.sif
 
 source /users/fquareng/.bashrc
-micromamba run -n dwnscl python /work/FAC/FGSE/IDYST/tbeucler/downscaling/fquareng/AdaptationSandbox/src/train.py 7
+micromamba run -n dwnscl python /work/FAC/FGSE/IDYST/tbeucler/downscaling/fquareng/WeatherAdaptSR/main.py
